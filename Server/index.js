2dfc0c10ef0faf4bf344925ce909451acf4acbcf
Siddhant <sonawane2582@gmail.com>
2024-07-06 13:59:00 +0530

    backend cors changes

--- Server/index.js
+++ Server/index.js
@@ -29,26 +29,14 @@ dotenv.config()
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:3000/', 'https://vibe-share-chi.vercel.app/'];
-
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
-
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
 app.use(morgan("common"))
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, "/public/assets")))
-
 
 /* FILE STORAGE CONFIGURATIONS */
 
