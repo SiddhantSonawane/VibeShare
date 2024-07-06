0c1aaae7cb86ca79cb4248e738c421425366d0d6
Siddhant <sonawane2582@gmail.com>
2024-07-06 14:26:01 +0530

    changes in frontend for url construction

--- Client/src/components/Friend.jsx
+++ Client/src/components/Friend.jsx
@@ -6,90 +6,92 @@ import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 import { useNavigate } from "react-router-dom";
 
-const Friend = ({friendId, name, subtitle, userPicturePath, showIcon}) => {
-    
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const { _id } = useSelector((state) => state.user)
-    const token = useSelector((state) => state.token)
-    const friends = useSelector((state) => state.user.friends)
-    
-    const {palette} = useTheme()
-    const primaryLight = palette.primary.light;
-    const primaryDark = palette.primary.dark 
-    const medium = palette.neutral.medium
-    const main = palette.neutral.main
+const Friend = ({ friendId, name, subtitle, userPicturePath, showIcon }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
 
-    let isFriend = null
-    
-    if(friends.length > 0)
-        isFriend = friends.find((friend) => friend._id === friendId)
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
 
-    let URL = process.env.REACT_APP_API_URL;
+  let isFriend = null;
 
-    const patchFriend = async () => {
-        if(_id === friendId){
-            alert("Don't be this much Introvert! We know you are already your friend...😂")
-        }
-        if(process.env.REACT_DEPLOYMENT === "PRODUCTION") {
-            URL = process.env.REACT_APP_API_URL
-        }  
-        const response = await fetch(`${URL}user/${_id}/${friendId}`, 
-            {
-                method: "PATCH",
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-type": "application/json"
-                }
-            }
-        )
-        const data = await response.json()
-        dispatch(setFriends({ friends: data}))
+  if (friends.length > 0)
+    isFriend = friends.find((friend) => friend._id === friendId);
+
+  let URL = process.env.REACT_APP_API_URL;
+
+  const patchFriend = async () => {
+    if (_id === friendId) {
+      alert(
+        "Don't be this much Introvert! We know you are already your friend...😂"
+      );
     }
+    if (process.env.REACT_DEPLOYMENT === "PRODUCTION") {
+      URL = process.env.REACT_APP_API_URL;
+    }
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/user/${_id}/${friendId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
 
+  return (
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={userPicturePath} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: primaryLight,
+                cursor: "pointer",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
 
-    return (
-        <FlexBetween>
-            <FlexBetween gap="1rem">
-                <UserImage image={userPicturePath} size="55px" />
-                <Box
-                    onClick={() => {
-                        navigate(`/profile/${friendId}`)
-                        navigate(0)
-                    }}
-                >
-                    <Typography
-                        color={main}
-                        variant="h5"
-                        fontWeight="500"
-                        sx = {{
-                            "&:hover":{
-                                color: primaryLight,
-                                cursor: "pointer"
-                            }
-                        }}
-                    >
-                        {name}
-                    </Typography>
-                    <Typography color={medium} fontSize="0.75rem">
-                        {subtitle}
-                    </Typography>
-                </Box>
-            </FlexBetween>
-            
-            { showIcon &&
-                <IconButton
-                    onClick={() => patchFriend()}
-                    sx = {{ backgroundColor: primaryLight, p: "0.6rem" }}
-                >
-                    {isFriend ? 
-                        (<PersonRemoveOutlined sx = {{ color: primaryDark }} />) :
-                        (<PersonAddOutlined sx = {{ color: primaryDark }} />)
-                    }
-                </IconButton>
-            }
-        </FlexBetween>
-    )
-}
+      {showIcon && (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      )}
+    </FlexBetween>
+  );
+};
 
-export default Friend
+export default Friend;
